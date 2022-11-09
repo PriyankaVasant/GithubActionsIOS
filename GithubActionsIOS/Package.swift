f6e8c51691b3a2ca70f4acce0ae15f869661e91b
priyanka Vasant <priyanka.vasant@mindinventory.com>
2022-11-09 13:03:50 +0530

    add package.swift file

---
+++ GithubActionsIOS/Package.swift
@@ -0,0 +1,18 @@
+//
+//  Package.swift
+//  GithubActionsIOS
+//
+//  Created by mac-0009 on 09/11/22.
+//
+
+import Foundation
+import PackageDescription
+
+
+let package = Package(
+    name: "YOUR_PROJECT_NAME",
+    products: [],
+    dependencies: [
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardManager.git", from: "6.5.0")
+    ]
+)
